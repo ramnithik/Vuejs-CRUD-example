6bb34d71748b6840109dd68dd71f995acdb6c5fd
RAMAR T <ramlax143@gmail.com>
2019-12-21 12:32:36 +0530

    Vuejs CRUD Application

---
+++ router/api.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var app = express();
+var appRoutes = express.Router();
+
+// Require Item model in our routes module
+var Item = require('../models/Item');
+
+// Defined store route
+appRoutes.route('/add').post(function (req, res) {
+  var item = new Item(req.body);
+      item.save()
+    .then(item => {
+    res.status(200).json({'item': 'Item added successfully'});
+    })
+    .catch(err => {
+    res.status(400).send("unable to save to database");
+    });
+});
+
+// Defined get data(index or listing) route
+appRoutes.route('/').get(function (req, res) {
+  Item.find(function (err, items){
+    if(err){
+      console.log(err);
+    }
+    else {
+      res.json(items);
+    }
+  });
+});
+
+// Defined edit route
+appRoutes.route('/edit/:id').get(function (req, res) {
+  var id = req.params.id;
+  Item.findById(id, function (err, item){
+      res.json(item);
+  });
+});
+
+//  Defined update route
+appRoutes.route('/update/:id').post(function (req, res) {
+  Item.findById(req.params.id, function(err, item) {
+    if (!item)
+      return next(new Error('Could not load Document'));
+    else {
+      item.name = req.body.name;
+      item.price = req.body.price;
+
+      item.save().then(item => {
+          res.json('Update complete');
+      })
+      .catch(err => {
+            res.status(400).send("unable to update the database");
+      });
+    }
+  });
+});
+
+// Defined delete | remove | destroy route
+appRoutes.route('/delete/:id').get(function (req, res) {
+  Item.findByIdAndRemove({_id: req.params.id}, function(err, item){
+        if(err) res.json(err);
+        else res.json('Successfully removed');
+    });
+});
+
+module.exports = appRoutes;
